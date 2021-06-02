5ddbe3fa35ff91cf18ed26acc06f6debb8c49b82
Ivan Ayala <network.9961@gmail.com>
2021-06-02 18:27:42 -0500

    viewmodel progress

---
+++ AFND/ViewModels/AfndViewModel.cpp
@@ -0,0 +1,11 @@
+#include "AfndViewModel.h"
+
+UI::AfndViewModel::AfndViewModel(QObject* parent)
+{
+
+}
+
+void UI::AfndViewModel::PrintData()
+{
+	qDebug() << "Se ha presionado el boton";
+}
--- AFND/ViewModels/AfndViewModel.h
+++ AFND/ViewModels/AfndViewModel.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef AFNDVIEWMODEL_H
+#define AFNDVIEWMODEL_h
+
+#include <QObject>
+#include <QDebug>
+
+namespace UI
+{
+	class AfndViewModel : public QObject
+	{
+		Q_OBJECT;
+
+	public:
+		explicit AfndViewModel(QObject* parent = nullptr);
+
+		Q_INVOKABLE void PrintData();
+
+	signals:
+
+	public slots:
+	};
+}
+
+#endif
